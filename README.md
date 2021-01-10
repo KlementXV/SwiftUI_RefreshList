10a21e169b9c017abf7bf375f5108029ea8d83a1
Zyg0matik <50872057+Zyg0matik@users.noreply.github.com>
2021-01-10 23:42:47 +0100

    up

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ struct ContentView: View {
     @State var Data:[Int] = [1,2,3]
 
     var body: some View {
-        AutoListRefresh(action: { AddData() } ){
+        ListRefresh(action: { AddData() } ){
             List {
                 ForEach(Data, id: \.self){ number in
                     Text("\(number)")
@@ -32,32 +32,6 @@ struct ContentView: View {
     }
     func AddData(){
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            withAnimation{ Data += [Data.count+1] }
-        }
-    }
-}
-```
-
-```
-import SwiftUI
-import RefreshList
-
-struct ContentView: View {
-    
-    @State var isShowing = false
-    @State var Data:[Int] = [1,2,3]
-
-    var body: some View {
-        ListRefresh(show: $isShowing, action: { AddData() } ){
-            List {
-                ForEach(Data, id: \.self){ number in
-                    Text("\(number)")
-                }
-            }
-        }
-    }
-    func AddData(){
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
             withAnimation{ Data += [Data.count+1] }
         }
     }
--- Sources/RefreshList/RefreshList.swift
+++ Sources/RefreshList/RefreshList.swift
@@ -3,7 +3,7 @@ import Introspect
 import ActionClosurable
 
 
-public struct AutoListRefresh<Content: View>: View {
+public struct ListRefresh<Content: View>: View {
     let content: () -> Content
     let action: () -> Void
     
@@ -18,36 +18,8 @@ public struct AutoListRefresh<Content: View>: View {
                 self.content()
                     .introspectTableView { tableView in
                         tableView.refreshControl = UIRefreshControl { refreshControl in
-                            self.action()
-                        }
-                    }
-            }
-        }
-    }
-}
-
-public struct ListRefresh<Content: View>: View {
-    @Binding var show: Bool
-    let content: () -> Content
-    let action: () -> Void
-    
-    public init(show: Binding<Bool>, action: @escaping () -> Void, @ViewBuilder content: @escaping () -> Content) {
-        _show = show
-        self.action = action
-        self.content = content
-    }
-    
-    public var body : some View {
-        GeometryReader { geometry in
-            ZStack(alignment: .center) {
-                self.content()
-                    .introspectTableView { tableView in
-                        tableView.refreshControl = UIRefreshControl { refreshControl in
-                            show = true
-                            refreshControl.beginRefreshing()
-
-                            self.action()
-                            if show == false {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                withAnimation { self.action() }
                                 refreshControl.endRefreshing()
                             }
                         }

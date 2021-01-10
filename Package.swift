9836a2aecfa9f6874735d4ca2114d9b307363947
Clément <50872057+clementlvx@users.noreply.github.com>
2021-01-10 16:18:01 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.0"),
-        .package(name: "ActionClosurable", url: "https://github.com/takasek/ActionClosurable.git", from: "2.1.0"),
+        .package(name: "Introspect", url: "https://github.com/clementlvx/SwiftUI-Introspect.git", from: "0.1.0"),
+        .package(name: "ActionClosurable", url: "https://github.com/clementlvx/ActionClosurable.git", from: "2.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
